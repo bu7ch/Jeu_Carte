66183d624f8e5e38ecd09f042921a64477dfcde3
Lee-sinX200 <dutchr@live.fr>
2015-06-25 22:28:11 +0200

    modif val & coul

--- Carte.py
+++ Carte.py
@@ -1,36 +1,17 @@
 class Carte:
-	def __init__(self, val, coul):
-		if val < 2 or val > 14:
-			print("Erreur :	La valeur d'une carte est comprise entre 2 et 14")
-		exit(1)
-		if coul < 0 or coul > 3:
-			print("Erreur: Le code couleur d'une carte est comprise entre 0 et 3")
-		exit(1)
-		self.valeur = val
-		self.couleur = coul
+    valeurs = (None, None,
+2, 3, 4, 5, 6, 7, 8, 9, 10, "Valet", "Dame", "Roi", "As")
+    couleurs = ("Coeur", "Carreau", "Trefle", "Pique")
 
-	def affiche(self):
-		affiche_valeur = None
-		affiche_couleur = None
+def __init__(self, val, coul):
+	if val < 2 or val > 14:
+		print("Erreur :	La valeur d'une carte est comprise entre 2 et 14")
+	exit(1)
+	if coul < 0 or coul > 3:
+		print("Erreur: Le code couleur d'une carte est comprise entre 0 et 3")
+	exit(1)
+	self.valeur = val
+	self.couleur = coul
 
-		if self.valeur <= 10:
-			affiche_valeur = self.valeur
-		elif self.valeur == 11:
-			affiche_valeur = "Valet"
-		elif self.valeur == 12:
-			affiche_valeur = "Dame"
-		elif self.valeur == 13:
-			affiche_valeur = "Roi"
-		else:
-			affiche_valeur = "As"
-
-		if self.couleur == 0:
-			affiche_couleur = "Coeur"
-		elif self.couleur == 1:
-			affiche_couleur = "Carreaux"
-		elif self.couleur == 2:
-			affiche_couleur = "Trefle"
-		else:
-			affiche_couleur = "Pique"
-
-		print(affiche_valeur, "de", affiche_couleur)
+def affiche(self):
+	print(Carte.valeurs[self.valeur], "de", Carte.couleurs[self.couleur])
